ab7d9bf95dbd38663899a6bc69fddb863885cb7c
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 11:21:31 +0300

    Added mongo connection

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
+
+mongoose.connect(process.env.MONGODB_URL).catch(err => {
+    console.error(err);
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
