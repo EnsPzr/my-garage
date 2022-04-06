aefbc43e2214172ce4ba8ac0f30e02600e06a1ad
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 13:19:33 +0300

    Added car update function

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -1,38 +1,35 @@
 const Car = require("../models/CarModel")
 const response = require("../utils/response")
 const validate = require("../utils/validate")
-const { validationResult } = require('express-validator');
+const {validationResult} = require('express-validator');
 
 exports.carGetAll = async function (req, res) {
     try {
         const rows = await Car.find({})
         return response.successResponse(res, rows)
     } catch (e) {
-        return response.errorResponse(res,e.message,500)
+        return response.errorResponse(res, e.message, 500)
     }
 }
 
 exports.carGet = async function (req, res) {
     try {
         const id = req.params.id;
-        if (!validate.ValidateObjectId(id))
-            return response.errorResponse(res,"Id format is not correct",400)
+        if (!validate.ValidateObjectId(id)) return response.errorResponse(res, "Id format is not correct", 400)
 
         const car = await Car.findById(id);
-        if (!car)
-            return response.errorResponse(res,"Car not found", 404)
+        if (!car) return response.errorResponse(res, "Car not found", 404)
 
         return response.successResponse(res, car)
     } catch (e) {
-        return response.errorResponse(res,e.message,500)
+        return response.errorResponse(res, e.message, 500)
     }
 }
 
 exports.carPost = async function (req, res) {
     try {
         const errors = validationResult(req);
-        if (!errors.isEmpty())
-            return response.errorResponse(res,errors.array().map(d => d.msg).join("<br>"),400);
+        if (!errors.isEmpty()) return response.errorResponse(res, errors.array().map(d => d.msg).join("<br>"), 400);
 
         const car = new Car({
             brand: req.body.brand,
@@ -43,33 +40,56 @@ exports.carPost = async function (req, res) {
         })
 
         const exResult = await Car.exists({number_plate: car.number_plate})
-        if (exResult)
-            return response.errorResponse(res,"This number plate already exist!",400)
+        if (exResult) return response.errorResponse(res, "This number plate already exist!", 400)
 
         await Car.create(car)
-        return response.successResponse(res,car)
+        return response.successResponse(res, car)
     } catch (e) {
-        response.errorResponse(res,e.message,500)
+        response.errorResponse(res, e.message, 500)
     }
 }
 
-exports.carPut = function (req, res) {
-    const id = req.params.id
-    response.successResponse(res,"Car Put Endpoint")
+exports.carPut = async function (req, res) {
+    try {
+        const id = req.params.id
+        if (!validate.ValidateObjectId(id)) return response.errorResponse(res, "Id format is not correct", 400)
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) return response.errorResponse(res, errors.array().map(d => d.msg).join("<br>"), 400);
+
+        const car = await Car.findById(id);
+        if (!car) return response.errorResponse(res, "Car not found", 404)
+
+        car.brand = req.body.brand
+        car.model = req.body.model
+        car.year = req.body.year
+        car.km = req.body.km
+        car.number_plate = req.body.number_plate.replace(/ /g, "")
+
+        const exResult = await Car.exists({
+            $and: [{number_plate: car.number_plate}, {_id: {$ne: id}}]
+        })
+        if (exResult) return response.errorResponse(res, "This number plate already exist!", 400)
+
+        const result = await Car.updateOne({_id: id}, car)
+        if (!result) return response.errorResponse(res, "Car not found", 404)
+
+        return response.successResponse(res, car)
+    } catch (e) {
+        response.errorResponse(res, e.message, 500)
+    }
 }
 
 exports.carDelete = async function (req, res) {
     try {
         const id = req.params.id
-        if (!validate.ValidateObjectId(id))
-            return response.errorResponse(res,"Id format is not correct",400)
+        if (!validate.ValidateObjectId(id)) return response.errorResponse(res, "Id format is not correct", 400)
 
         const result = await Car.findByIdAndDelete({_id: id})
-        if (!result)
-            return response.errorResponse(res,"Car not found",404)
+        if (!result) return response.errorResponse(res, "Car not found", 404)
 
-        return response.successResponse(res,id)
+        return response.successResponse(res, id)
     } catch (e) {
-        response.errorResponse(res,e.message,500)
+        response.errorResponse(res, e.message, 500)
     }
 }
