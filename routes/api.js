823dd0f3b8997ae31ef527492e190e117269fce7
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 14:51:52 +0300

    Added auth routes in api routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const authRoutes = require("./auth");
 const carRoutes = require("./car");
 
 const app = express();
+app.use("/auth/", authRoutes)
 app.use("/car/",carRoutes);
 
 module.exports = app;
