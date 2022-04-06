60809dd6af9d0c9edfa962873b19163db4469715
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 11:49:15 +0300

    Added car get function

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -1,6 +1,6 @@
 const Car = require("../models/CarModel")
 const response = require("../utils/response")
-
+const validate = require("../utils/validate")
 
 exports.carGetAll = async function (req, res) {
     try {
@@ -11,9 +11,22 @@ exports.carGetAll = async function (req, res) {
     }
 }
 
-exports.carGet = function (req, res) {
-    const id = req.params.id
-    response.successResponse(res,"Car Get Endpoint")
+exports.carGet = async function (req, res) {
+    try {
+        const id = req.params.id;
+        if (!validate.ValidateObjectId(id)) {
+            response.errorResponse(res,"Id format is not correct",400)
+            return;
+        }
+        const car = await Car.findById(id);
+        if (!car) {
+            response.errorResponse(res,"Car not found", 404)
+            return
+        }
+        response.successResponse(res, car)
+    } catch (e) {
+        response.errorResponse(res,e.message,500)
+    }
 }
 
 exports.carPost = function (req, res) {
