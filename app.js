976451369938c26a3a1ca6f2b83dd6e2894c55f0
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 14:51:13 +0300

    Added global error middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const routes = require("./routes/api")
 const mongoose = require('mongoose');
-
+const response = require("./utils/response")
 app.use(express.json())
 
 mongoose.connect(process.env.MONGODB_URL).catch(err => {
@@ -10,6 +10,12 @@ mongoose.connect(process.env.MONGODB_URL).catch(err => {
 })
 
 app.use("/api/", routes)
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        response.errorResponse(res,"You must logging",401)
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
