c84ad8502a3bfb9f8fe503278064cb770b8c7f91
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 12:52:46 +0300

    Added validation function in route

--- routes/car.js
+++ routes/car.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const carController = require("../controllers/CarController")
+const {carValidation} = require("../utils/validate")
 const router = express.Router()
 
 router.get("/", carController.carGetAll)
 router.get("/:id", carController.carGet)
-router.post("/", carController.carPost)
+router.post("/", carValidation, carController.carPost)
 router.put("/:id", carController.carPut)
-router.delete(":/id",carController.carDelete)
+router.delete(":/id", carController.carDelete)
 
 module.exports = router;
