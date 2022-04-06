af4d27b8088b16cce1ce319375cee5552decba84
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 12:55:01 +0300

    Added car post function, clear excess curly brackets

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -1,36 +1,56 @@
 const Car = require("../models/CarModel")
 const response = require("../utils/response")
 const validate = require("../utils/validate")
+const { validationResult } = require('express-validator');
 
 exports.carGetAll = async function (req, res) {
     try {
         const rows = await Car.find({})
-        response.successResponse(res, rows)
+        return response.successResponse(res, rows)
     } catch (e) {
-        response.errorResponse(res,e.message,500)
+        return response.errorResponse(res,e.message,500)
     }
 }
 
 exports.carGet = async function (req, res) {
     try {
         const id = req.params.id;
-        if (!validate.ValidateObjectId(id)) {
-            response.errorResponse(res,"Id format is not correct",400)
-            return;
-        }
+        if (!validate.ValidateObjectId(id))
+            return response.errorResponse(res,"Id format is not correct",400)
+
         const car = await Car.findById(id);
-        if (!car) {
-            response.errorResponse(res,"Car not found", 404)
-            return
-        }
-        response.successResponse(res, car)
+        if (!car)
+            return response.errorResponse(res,"Car not found", 404)
+
+        return response.successResponse(res, car)
     } catch (e) {
-        response.errorResponse(res,e.message,500)
+        return response.errorResponse(res,e.message,500)
     }
 }
 
-exports.carPost = function (req, res) {
-    response.successResponse(res,"Car Post Endpoint")
+exports.carPost = async function (req, res) {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return response.errorResponse(res,errors.array().map(d => d.msg).join("<br>"),400);
+
+        const car = new Car({
+            brand: req.body.brand,
+            model: req.body.model,
+            year: req.body.year,
+            km: req.body.km,
+            number_plate: req.body.number_plate.replace(/ /g, "")
+        })
+
+        const exResult = await Car.exists({number_plate: car.number_plate})
+        if (exResult)
+            return response.errorResponse(res,"This number plate already exist!",400)
+
+        await Car.create(car)
+        return response.successResponse(res,car)
+    } catch (e) {
+        response.errorResponse(res,e.message,500)
+    }
 }
 
 exports.carPut = function (req, res) {
