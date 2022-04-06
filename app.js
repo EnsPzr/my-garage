3e26c712bcc1657c16661dde6df53e4ed305aeff
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 11:32:10 +0300

    Added router in app file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const routes = require("./routes/api")
 const mongoose = require('mongoose');
 
 
 mongoose.connect(process.env.MONGODB_URL).catch(err => {
     console.error(err);
 })
+
+app.use("/api/", routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
