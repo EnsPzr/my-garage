be940b14c17172e5db9122b0af11f12c9b7fafc1
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 14:50:36 +0300

    Added login and register validation and changed validation messages

--- utils/validate.js
+++ utils/validate.js
@@ -6,10 +6,23 @@ exports.ValidateObjectId = function (id) {
 
 const year = new Date().getFullYear()
 exports.carValidation = [
-    body("brand").isLength({min: 2}).withMessage("Marka en az 2 karakter olmalıdır").trim(),
-    body("model").isLength({min: 2}).withMessage("Model en az 2 karakter olmalıdır").trim(),
-    body("year").isInt({min: 1950, max: year}).withMessage(`Yıl en az 1950, en fazla ${year} olabilir`),
-    body("km").isNumeric().withMessage("Km sadece rakamlardan oluşabilir").isLength({min: 0}).withMessage("Km en az 0 olabilir"),
+    body("brand").trim().isLength({min: 2}).withMessage("Brand be minimum 2 character"),
+    body("model").trim().isLength({min: 2}).withMessage("Model be minimum 2 character"),
+    body("year").isInt({min: 1950, max: year}).withMessage(`Year must be between 1950-${year}`),
+    body("km").isNumeric().withMessage("Km must be only numeric characters"),
     // TODO: Replace don't working?
-    body("number_plate").notEmpty().withMessage("Plaka zorunlu bir alandır").replace(/ /g, "")
+    body("number_plate").notEmpty().withMessage("Number plate is required").replace(/ /g, "")
+]
+
+exports.loginValidate = [
+    body("email").trim().isLength({min: 2}).withMessage("Email be minimum 2 character").isEmail().withMessage("Please enter the correct format email"),
+    body("password").trim().isLength({min: 6}).withMessage("Password be minimum 6 character"),
+]
+
+
+exports.registerValidate = [
+    body("email").trim().isLength({min: 2}).withMessage("Email be minimum 2 character").isEmail().withMessage("Please enter the correct format email"),
+    body("password").trim().isLength({min: 6}).withMessage("Passwrod be minimum 6 character"),
+    body("firstname").trim().isLength({min:2}).withMessage("First name be minimum 2 character"),
+    body("lastname").trim().isLength({min:2}).withMessage("Last name be minimum 2 character"),
 ]
