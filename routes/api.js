f49f11ac6b88590bf786e791c4e76a34479c53d6
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 20:56:53 +0300

    Added file upload function in routes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const authRoutes = require("./auth");
 const carRoutes = require("./car");
+const uploadController = require("../controllers/UploadController")
 
 const app = express();
+
+app.post("/upload", uploadController.uploadFile)
 app.use("/auth/", authRoutes)
 app.use("/car/",carRoutes);
 
