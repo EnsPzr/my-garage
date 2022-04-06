76263d201591d54279ce70e6d30a88e706175b55
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-06 11:20:55 +0300

    Backend coding started

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+    console.log("Backend Started...")
+})
